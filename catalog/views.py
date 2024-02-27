a66fcea492e502058018b71b55b157c8b322240d
Akimenko Ivan <104264107+NUDA9A@users.noreply.github.com>
2024-02-27 09:44:50 +0300

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'catalog/index.html')
-
-
 def home(request):
     return render(request, 'catalog/home.html')
 
